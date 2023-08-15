7efb3cc1ecab10df898f0cf6464c35cd3ec17d76
Benjamin Wuethrich <benjamin.wuethrich@gmail.com>
2023-08-15 17:33:02 -0400

    Refactor a bit

--- toy.js
+++ toy.js
@@ -1,68 +1,36 @@
+class Point {
+	constructor(x, y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	set(x, y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	move(dx, dy) {
+		this.x += dx;
+		this.y += dy;
+	}
+
+	moveX(dx) {
+		this.x += dx;
+	}
+
+	moveY(dy) {
+		this.y += dy;
+	}
+}
+
+class Scene {
+	constructor() {
+		this.point = new Point(innerWidth / 2, innerHeight / 2);
+		this.orientation = [0, 0, 0, 0];
+	}
+}
+
 function init() {
-	let canvas = document.querySelector("canvas");
-	canvas.width = innerWidth;
-	canvas.height = innerHeight;
-	let ctx = canvas.getContext("2d");
-	return [canvas, ctx];
-}
-
-function updateScene(ctx, x, y, r, sensor) {
-	ctx.clearRect(0, 0, innerWidth, innerHeight);
-	ctx.beginPath();
-	ctx.arc(x, y, r, 0, 2 * Math.PI);
-	ctx.fill();
-	ctx.textAlign = "end";
-	ctx.fillText(`x: ${x}`, innerWidth - 10, 20);
-	ctx.fillText(`y: ${y}`, innerWidth - 10, 30);
-	ctx.textAlign = "left";
-	sensor.quaternion.forEach((el, idx) => {
-		ctx.fillText(`[${idx}]: ${el}`, 10, innerHeight - 50 + idx * 10);
-	})
-}
-
-function frame(time) {
-	updateScene(ctx, x, y, r, sensor);
-	requestAnimationFrame(frame);
-}
-
-addEventListener("keydown", event => {
-	let step = 2 * r;
-	switch (event.key) {
-		case 'ArrowRight':
-			x = x + step > innerWidth ? x : x + step;
-			break;
-		case 'ArrowLeft':
-			x = x - step < 0 ? x : x - step;
-			break;
-		case 'ArrowUp':
-			y = y - step < 0 ? y : y - step;
-			break;
-		case 'ArrowDown':
-			y = y + step > innerHeight ? y : y + step;
-			break;
-	}
-});
-
-function setSize() {
-	canvas.width = innerWidth;
-	canvas.height = innerHeight;
-	x = Math.min(x, innerWidth);
-	y = Math.min(y, innerHeight);
-}
-
-addEventListener("resize", () => setSize());
-
-function requestFullscreen() {
-	if (!document.fullscreenElement) {
-		document.documentElement.requestFullscreen().catch((err) => {
-			console.warn(`Enabling fullscreen mode: ${err.message} (${err.name})`)
-		});
-	}
-}
-
-addEventListener("pointerdown", () => requestFullscreen());
-
-function getSensor() {
 	const sensor = new AbsoluteOrientationSensor();
 	Promise.all([
 		navigator.permissions.query({ name: "accelerometer" }),
@@ -76,12 +44,66 @@ function getSensor() {
 		}
 	});
 
-	return sensor;
+	let canvas = document.querySelector("canvas");
+	canvas.width = innerWidth;
+	canvas.height = innerHeight;
+
+	return canvas.getContext("2d");
 }
 
-let [canvas, ctx] = init();
+function paintScene() {
+	ctx.clearRect(0, 0, innerWidth, innerHeight);
+	ctx.beginPath();
+	ctx.arc(scene.point.x, scene.point.y, r, 0, 2 * Math.PI);
+	ctx.fill();
+	ctx.textAlign = "end";
+	ctx.fillText(`x: ${scene.point.x}`, innerWidth - 10, 20);
+	ctx.fillText(`y: ${scene.point.y}`, innerWidth - 10, 30);
+	ctx.textAlign = "left";
+	scene.orientation.forEach((el, idx) => {
+		ctx.fillText(`[${idx}]: ${el}`, 10, innerHeight - 50 + idx * 10);
+	});
+}
+
+function frame() {
+	paintScene();
+	requestAnimationFrame(frame);
+}
+
+function updateScene(event) {
+	let step = 2 * r;
+	let x = scene.point.x;
+	let y = scene.point.y;
+	switch (event.key) {
+	case "ArrowRight":
+		scene.point.x = x + step > innerWidth ? x : x + step;
+		break;
+	case "ArrowLeft":
+		scene.point.x = x - step < 0 ? x : x - step;
+		break;
+	case "ArrowUp":
+		scene.point.y = y - step < 0 ? y : y - step;
+		break;
+	case "ArrowDown":
+		scene.point.y = y + step > innerHeight ? y : y + step;
+		break;
+	}
+}
+
+addEventListener("keydown", updateScene);
+
+function setSize() {
+	let canvas = document.querySelector("canvas");
+	canvas.width = innerWidth;
+	canvas.height = innerHeight;
+	scene.point.set(Math.min(scene.point.x, innerWidth),
+		Math.min(scene.point.y, innerHeight));
+}
+
+addEventListener("resize", () => setSize());
+
+let ctx = init();
+let scene = new Scene();
 const r = 5;
-let x = innerWidth / 2, y = innerHeight / 2;
-let sensor = getSensor();
 
 requestAnimationFrame(frame);
