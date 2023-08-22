6a0cb9e88ca23f61e0a0f64280a4b5b39566ccda
Benjamin Wuethrich <benjamin.wuethrich@gmail.com>
2023-08-22 17:55:29 -0400

    Adjust scaling

--- toy2d.js
+++ toy2d.js
@@ -11,14 +11,15 @@ class Point {
 	}
 
 	update(F, dt) {
+		const scaleBy = 120; // compensate for 1 pixel = 1 metre
 		let pNew = {
 			x: this.p.x + this.v.x * dt + this.a.x * dt**2 * 0.5,
 			y: this.p.y + this.v.y * dt + this.a.y * dt**2 * 0.5
 		};
 		let aNew = {
 			// scale by 100
-			x: 100 * F.x / this.m,
-			y: 100 * F.y / this.m
+			x: scaleBy * F.x / this.m,
+			y: scaleBy * F.y / this.m
 		};
 		let vNew = {
 			x: this.v.x + (this.a.x + aNew.x) * (dt * 0.5),
